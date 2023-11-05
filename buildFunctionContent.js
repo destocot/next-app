587e2ba7626276ecb2198f0686ed07eba27086c8
destocot <khurramcali@gmail.com>
2023-11-04 20:57:34 -0400

    fixed products

---
+++ buildFunctionContent.js
@@ -0,0 +1,22 @@
+const matter = require('gray-matter')
+const fs = require('node:fs')
+
+const getProducts = () => {
+  const directory = `${process.cwd()}/content`
+  const filenames = fs.readdirSync(directory)
+
+  const products = filenames.map((filename) => {
+    // read the file from fs
+    const fileContent = fs.readFileSync(`${directory}/${filename}`).toString()
+    // pull out frontmatter => name
+    const { data } = matter(fileContent)
+
+    return data
+  })
+
+  return products
+}
+
+const filePath = `${process.cwd()}/functions/products.json`
+const products = getProducts()
+fs.writeFileSync(filePath, JSON.stringify(products))
